87c37284d491a1961d4b6516b10f637930e19e55
Richard Ward <richard@richard.ward.name>
2020-04-13 12:09:10 +0100

    Switch to older JS to support shitty browsers

--- loerm.js
+++ loerm.js
@@ -1,9 +1,9 @@
-(() => {
-    const appCanvas = document.querySelector('canvas.app-canvas');
-    const context = appCanvas.getContext('2d');
+(function () {
+    var appCanvas = document.querySelector('canvas.app-canvas');
+    var context = appCanvas.getContext('2d');
 
     function initializeCanvasSizing() {
-        const appCanvasContainer = document.querySelector('.app-canvas-container');
+        var appCanvasContainer = document.querySelector('.app-canvas-container');
 
         function resizeCanvas() {
             appCanvas.width = Math.max(100, appCanvasContainer.offsetWidth);
@@ -22,54 +22,53 @@
 
     initializeCanvasSizing();
 
-    appCanvas.addEventListener("mousemove", (mousemove) => {
-        const {movementX, movementY, offsetX, offsetY} = mousemove;
+    appCanvas.addEventListener("mousemove", function (mousemove) {
         return onInputMove({
-            offsetX: offsetX,
-            offsetY: offsetY,
-            movementX: movementX,
-            movementY: movementY,
+            offsetX: mousemove.offsetX,
+            offsetY: mousemove.offsetY,
+            movementX: mousemove.movementX,
+            movementY: mousemove.movementY
         });
     });
 
-    let previousTouches;
+    var previousTouches;
 
-    appCanvas.addEventListener("touchstart", (touchStart) => {
+    appCanvas.addEventListener("touchstart", function (touchStart) {
         resetPreviousTouches(touchStart);
     });
 
-    appCanvas.addEventListener("touchmove", (touchMove) => {
-        const TOUCH_OFFSET = 48; // allow people to see without their thumb/finger in the way
-        const touch = touchMove.touches[0];
-        const previousTouch = previousTouches[touch.identifier];
-        const offsetX = touch.clientX - appCanvas.offsetLeft - TOUCH_OFFSET;
-        const offsetY = touch.clientY - appCanvas.offsetTop - TOUCH_OFFSET;
-        const movementX = offsetX - (previousTouch.clientX - appCanvas.offsetLeft - TOUCH_OFFSET);
-        const movementY = offsetY - (previousTouch.clientY - appCanvas.offsetTop - TOUCH_OFFSET);
-        onInputMove({offsetX, offsetY, movementX, movementY});
+    appCanvas.addEventListener("touchmove", function (touchMove) {
+        var TOUCH_OFFSET = 48; // allow people to see without their thumb/finger in the way
+        var touch = touchMove.touches[0];
+        var previousTouch = previousTouches[touch.identifier];
+        var offsetX = touch.clientX - appCanvas.offsetLeft - TOUCH_OFFSET;
+        var offsetY = touch.clientY - appCanvas.offsetTop - TOUCH_OFFSET;
+        var movementX = offsetX - (previousTouch.clientX - appCanvas.offsetLeft - TOUCH_OFFSET);
+        var movementY = offsetY - (previousTouch.clientY - appCanvas.offsetTop - TOUCH_OFFSET);
+        onInputMove({offsetX: offsetX, offsetY: offsetY, movementX: movementX, movementY: movementY});
         resetPreviousTouches(touchMove);
     });
 
     function resetPreviousTouches(touchEvent) {
         previousTouches = {};
-        for (let i = 0; i < touchEvent.touches.length; i++) {
+        for (var i = 0; i < touchEvent.touches.length; i++) {
             previousTouches[touchEvent.touches[i].identifier] = touchEvent.touches[i];
         }
     }
 
-    let screenLetters = [];
+    var screenLetters = [];
 
-    const texts = [
+    var texts = [
         "To-morrow, and to-morrow, and to-morrow, Creeps in this petty pace from day to day, To the last syllable of recorded time; And all our yesterdays have lighted fools The way to dusty death. Out, out, brief candle! Life is but a walking shadow, a poor player That struts and frets his hour upon the stage And then is heard no more. It is a tale Told by an idiot, full of sound and fury Signifying nothing.",
         " Out, damn'd spot! out, I say!—One; two: why, then 'tis time to do't.—Hell is murky. —Fie, my lord, fie, a soldier, and afeard? What need we fear who knows it, when none can call our pow'r to accompt? —Yet who would have thought the old man to have had so much blood in him?",
         "O serpent heart hid with a flowering face! Did ever a dragon keep so fair a cave? Beautiful tyrant, feind angelical, dove feather raven, wolvish-ravening lamb! Despised substance of devinest show, just opposite to what thou justly seemest - A dammed saint, an honourable villain!",
-        "Villain, what hast thou done? That which thou canst not undo. Thou hast undone our mother. Villain, I have done thy mother.",
+        "Villain, what hast thou done? That which thou canst not undo. Thou hast undone our mother. Villain, I have done thy mother."
     ]
-    let textNumber = 0, characterNumber = 0;
-    let lastLetterAdded = window.performance.now() / 1000;
+    var textNumber = 0, characterNumber = 0;
+    var lastLetterAdded = window.performance.now() / 1000;
 
-    function onInputMove({offsetX, offsetY, movementX, movementY}) {
-        const now = window.performance.now() / 1000;
+    function onInputMove(moveInfo) {
+        var now = window.performance.now() / 1000;
         if (now - lastLetterAdded < 0.09) {
             return;
         }
@@ -79,7 +78,7 @@
             return Math.max(Math.min(values, max), -max);
         }
 
-        const character = texts[textNumber][characterNumber];
+        var character = texts[textNumber][characterNumber];
         characterNumber += 1;
         if (characterNumber >= texts[textNumber].length) {
             characterNumber = 0;
@@ -92,47 +91,49 @@
         screenLetters.push({
             character: character,
             velocity: {
-                x: absMax(movementX, 14),
-                y: absMax(movementY, 14),
+                x: absMax(moveInfo.movementX, 14),
+                y: absMax(moveInfo.movementY, 14)
             },
-            angularVelocity: absMax(movementX / 100, 0.06),
+            angularVelocity: absMax(moveInfo.movementX / 100, 0.06),
             position: {
-                x: offsetX,
-                y: offsetY,
+                x: moveInfo.offsetX,
+                y: moveInfo.offsetY
             },
-            angle: 0,
+            angle: 0
         });
     }
 
     function tick(timeInfo) {
-        const gravity = {
+        var gravity = {
             x: 0,
-            y: 10,
+            y: 10
         }
-        screenLetters.forEach(screenLetter => {
+        screenLetters.forEach(function (screenLetter) {
             screenLetter.velocity.x += gravity.x * timeInfo.delta;
             screenLetter.velocity.y += gravity.y * timeInfo.delta;
             screenLetter.position.x += screenLetter.velocity.x;
             screenLetter.position.y += screenLetter.velocity.y;
             screenLetter.angle += screenLetter.angularVelocity;
         })
-        screenLetters = screenLetters.filter(screenLetter => screenLetter.position.y < appCanvas.height * 1.5);
+        screenLetters = screenLetters.filter(function (screenLetter) {
+            return screenLetter.position.y < appCanvas.height * 1.5;
+        });
     }
 
 
     function render() {
         context.clearRect(0, 0, appCanvas.width, appCanvas.height);
 
-        screenLetters.forEach(screenLetter => {
-            let offset;
+        screenLetters.forEach(function (screenLetter) {
+            var offset;
             context.font = "2em sans-serif";
             context.save();
             context.translate(screenLetter.position.x, screenLetter.position.y);
             context.rotate(screenLetter.angle);
-            const textMetrics = context.measureText(screenLetter.character);
-            const glyphMetrics = {
+            var textMetrics = context.measureText(screenLetter.character);
+            var glyphMetrics = {
                 width: textMetrics.actualBoundingBoxLeft + textMetrics.actualBoundingBoxRight,
-                height: textMetrics.actualBoundingBoxAscent + textMetrics.actualBoundingBoxDescent,
+                height: textMetrics.actualBoundingBoxAscent + textMetrics.actualBoundingBoxDescent
             }
             context.fillStyle = 'black';
             offset = (isNaN(glyphMetrics.height) || isNaN(glyphMetrics.width))
@@ -145,15 +146,15 @@
     }
 
     function startFrameLoop() {
-        const initialTimeStamp = window.performance.now();
-        let lastTimeStamp = initialTimeStamp;
+        var initialTimeStamp = window.performance.now();
+        var lastTimeStamp = initialTimeStamp;
 
 
         function animationFrame(currentTimeStamp) {
             requestAnimationFrame(animationFrame);
-            const timeInfo = {
+            var timeInfo = {
                 time: (currentTimeStamp - initialTimeStamp) / 1000,
-                delta: (currentTimeStamp - lastTimeStamp) / 1000,
+                delta: (currentTimeStamp - lastTimeStamp) / 1000
             };
             tick(timeInfo);
             render();
